fe488d1474862074efccf10edae1c4f5ca6bbd54
Martell Howze <martellhowze042988@gmail.com>
2021-08-24 22:54:19 -0500

    commit tree c694f72f1d5f279f7628982ecad0b39a690f1887

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+//const drivers = function(){
+  //  return ['Sally', 'Bob', 'Freddy', 'Claudia'];
+//}
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    return function(value){
+        return num * value}
+    }
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (forest, driverFunction) =>{
+    return driverFunction(forest)
+}
